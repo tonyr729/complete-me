41ad90808d4a0c1bea5c208b24f9df6cfbda9629
Tony Robertson <tonyr729@gmail.com>
2018-04-04 10:48:00 -0600

    Add suggest method
    Added suggest methods.
    Added word count method.

--- scripts/Trie.js
+++ scripts/Trie.js
@@ -3,12 +3,17 @@ import Node from './Node';
 export default class Trie {
   constructor() {
     this.root = new Node();
+    this.wordCount = 0;
   }
 
   insert(word, currentNode = this.root) {
     console.log(word.substr(1));
     if (!word.length) {
-      this.isWord = true;
+      if (!currentNode.isWord) {
+        this.wordCount++;
+        currentNode.isWord = true;
+        return;
+      }
       return;
     } 
   
@@ -21,6 +26,34 @@ export default class Trie {
 
     this.insert(word.substr(1), currentNode)
   }
+
+  count(){
+    return this.wordCount
+  }
+
+  suggest(word, currentNode = this.root) {
+    this.suggestionArray = [];
+
+    for (let i = 0; i < word.length; i++) {
+      if (currentNode.childrenObj[word[i]]) {
+        currentNode = currentNode.childrenObj[word[i]];
+      }
+    }
+
+    this.getSuggestions(word, currentNode)
+    return this.suggestionArray;
+  }
+
+
+  getSuggestions(prefix, currentNode = this.root) {
+    
+    if (currentNode.isAWord) {
+      this.suggestionArray.push(prefix)
+    }
+
+    let letters = Object.keys(currentNode.childrenObj)
+    letters.forEach(letter => {
+      return this.getSuggestions(prefix + letter, currentNode.childrenObj[letter])
+    })
+  }
 }
-
-
